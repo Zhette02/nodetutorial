f42f798c6ae7d8cb85924bcf5a7d93fd11780dad
zhette02 <zhette23@gmail.com>
2023-11-06 17:56:14 -0500

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello")
+//import express library and assogn to variable
+import express from "express";
+
+//create instance of express application
+const app = express();
+
+//set the port the application will be running on
+const port = process.env.PORT || 3001;
+
+// set up a response for the root path of the application
+    app.get('/', (req, res) => {
+    res.send("draw someone or something you love")
+})
+
+// set the application to listen on the specified port
+app.listen(port, () => {
+    console.log(`example at listening on port ${port}`)
+})
+
+
+
